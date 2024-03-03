57460136926835fce7e3c352b2012e6a65660599
IssamQaci <128149288+IssamQaci@users.noreply.github.com>
2024-03-03 10:03:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let int = setInterval(() => {
+    let screenwidth = window.innerWidth;
+    console.log(screenwidth/2);
+    document.getElementById("noBtn").style.left = screenwidth/2 - 100 + "px" ;
+}, 7000);
+
+function leave(){
+    console.log(r);
+    if(r == 0){
+        document.getElementById("noBtn").style.marginTop = "100px";
+        r = 1;
+    }
+    else{
+        document.getElementById("noBtn").style.marginTop = "0px";
+        r = 0;
+    }
+}
+let r  = 1;
+function leave1(){
+    console.log(r);
+    if(r == 0){
+        document.getElementById("noBtn").style.marginTop = "100px";
+        r = 1;
+    }
+    else{
+        document.getElementById("noBtn").style.marginTop = "0px";
+        r = 0;
+    }
+}
